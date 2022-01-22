ee892dd40bbc718ac094288f2ad2a0dc79e0bf19
David Omar Ghiano <davidghiano@gmail.com>
2022-01-21 21:50:19 -0300

    proyecto finalizado

---
+++ 05-deseos/android/app/src/main/java/com/dogdev/deseos/MainActivity.java
@@ -0,0 +1,5 @@
+package com.dogdev.deseos;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- 05-deseos/src/app/components/listas/listas.component.ts
+++ 05-deseos/src/app/components/listas/listas.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
 import { DeseosService } from '../../services/deseos.service';
 import { Lista } from '../../models/lista.model';
 import { Router } from '@angular/router';
+import { AlertController, IonList } from '@ionic/angular';
 
 @Component({
   selector: 'app-listas',
@@ -9,12 +10,62 @@ import { Router } from '@angular/router';
   styleUrls: ['./listas.component.scss'],
 })
 export class ListasComponent {
-
+  @ViewChild( IonList ) lista: IonList;
+  @Input() terminada = true;
 
   constructor(public deseosService: DeseosService,
-              private router: Router ) { }
+              private router: Router,
+              private alertCtrl: AlertController ) { }
 
   listaSeleccionada( lista: Lista ){
-    this.router.navigateByUrl(`/tabs/tab1/agregar/${ lista.id }`);
+    if( this.terminada ){
+      this.router.navigateByUrl(`/tabs/tab2/agregar/${ lista.id }`);
+    }else{
+      this.router.navigateByUrl(`/tabs/tab1/agregar/${ lista.id }`);
+    }
   }
+
+  async editar( lista: Lista ){
+
+    const alert = await this.alertCtrl.create({
+      cssClass: 'my-custom-class',
+      header: 'Editar nombre de la Lista',
+      inputs: [
+        {
+          name: 'titulo',
+          type: 'text',
+          placeholder: lista.titulo
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancer',
+          handler: () => {
+            this.lista.closeSlidingItems();
+          }
+        },
+        {
+          text: 'Editar',
+          handler: ( data ) => {
+            if( data.titulo.length === 0 ){
+              return;
+            }
+
+            lista.titulo = data.titulo;
+            this.deseosService.guardarStorage();
+            this.lista.closeSlidingItems();
+          }
+        }
+      ]
+    });
+
+    alert.present();
+  }
+
+  borrarLista( lista: Lista ){
+    this.deseosService.borrarLista( lista );
+  }
+
+
 }
